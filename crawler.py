77ce7ed304a25164f2a5e5a3c3b187461d75bb4f
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2026-04-30 17:29:49 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,12 +1,5 @@
-from flask import Flask
 import requests
 
-app = Flask(__name__)
-
-@app.route("/crawl")
-def crawl():
-    url = "https://www.google.com/"
+def crawl(url):
     res = requests.get(url)
-    return res.text[:1000]  # 先頭だけ表示
-
-app.run()
+    return res.text[:500]
