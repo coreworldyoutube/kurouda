9f365a4c236efb31f860693eda448ed13d0be02d
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2026-05-05 20:06:31 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,5 +1,23 @@
 import requests
 
-def crawl(url):
-    res = requests.get(url)
-    return res.text[:500]
+ALLOWED_DOMAIN = "example.com"
+
+def fetch_code(url):
+    r = requests.get(url, timeout=5)
+    r.raise_for_status()
+    return r.text
+
+def is_safe(url):
+    return ALLOWED_DOMAIN in url
+
+def run_code(code):
+    # 危険：本来はsandbox必須
+    exec(code)
+
+url = "https://www.google.com/"
+
+if is_safe(url):
+    code = fetch_code(url)
+    run_code(code)
+else:
+    print("拒否: 許可されていないURL")
